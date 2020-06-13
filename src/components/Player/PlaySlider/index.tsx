f0fe76373a8c0ab58ac444b3aaa166fd126df131
Siarhei Drozd <drozd1586229@gmail.com>
2020-06-13 20:22:29 +0200

    fixed: slider

--- src/components/Player/PlaySlider/index.tsx
+++ src/components/Player/PlaySlider/index.tsx
@@ -5,47 +5,24 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./index.scss";
 
-import cover from "../../../shared/images/cover.png";
-import unrealesed_cover from "../../../shared/images/unreleased_cover.png";
-import cover_1 from "../../../shared/images/cover-1.png";
-
 export const PlaySlider: React.FC = () => {
-  const configuration = {
-  centerMode: true,
-  centerPadding: '20px',
-  slidesToShow: 1,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '10px',
-        slidesToShow: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '10px',
-        slidesToShow: 1
-      }
-    }
-  ]
-
+  const settings = {
+    centerMode: true,
+    centerPadding: "90px",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
   };
   return (
-    <Slider {...configuration}>
-      <div className="play-slider__element">
-        <img src={cover} alt="" />
+    <Slider {...settings}>
+      <div className="play-slider__element play-slider__element-1">
+        <div className="play-slider__logo"></div>
       </div>
-      <div className="play-slider__element">
-        <img src={unrealesed_cover} alt="" />
+      <div className="play-slider__element play-slider__element-2">
+        <div className="play-slider__logo"></div>
       </div>
-      <div className="play-slider__element">
-        <img src={cover_1} alt="" />
+      <div className="play-slider__element play-slider__element-3">
+        <div className="play-slider__logo"></div>
       </div>
     </Slider>
   );
--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -10,9 +10,29 @@ export const Player: React.FC = () => {
       <div className="player__slider">
         <PlaySlider />
       </div>
-      <div className="player__title"></div>
-      <div className="player__controls"></div>
-      <div className="player__playback-buffers"></div>
+      <div className="player__title">
+        <h4 className="player__playback-name">Self Concious</h4>
+        <h6 className="player__artist">KANYE WEST</h6>
+      </div>
+      <div className="player__controls">
+        <div className="player__controls-btn shuffle-btn"></div>
+        <div className="player__controls-btn prev-btn"></div>
+        <div className="player__controls-btn play-btn"></div>
+        <div className="player__controls-btn next-btn"></div>
+        <div className="player__controls-btn repeat-btn"></div>
+      </div>
+      <div className="player__playback-buffers">
+        <div className="player__buffer">
+          <div className="start-time">0.00</div>
+          <div className="track">
+            <div className="track-wide"></div>
+          </div>
+          <div className="end-time">3.27</div>
+        </div>
+        <div className="player__waveform-outer">
+          <div className="player__waveform-track"></div>
+        </div>
+      </div>
     </div>
   );
 };
